abd0d6450bc2c687a9db346113db3b654725bc75
mickeygoldmeier <45402517+mickeygoldmeier@users.noreply.github.com>
2020-03-17 11:33:32 +0200

    add classes

--- IceCreamKioskInformation/MainWindow.xaml.cs
+++ IceCreamKioskInformation/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace IceCreamKioskInformation
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
