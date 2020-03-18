cc98075f5dfe42b6317c1842fc0de89bfe0f4dfe
nmanor <45391273+nmanor@users.noreply.github.com>
2020-03-18 18:57:43 +0200

    Bring back the deleted files

---
+++ BE/Address.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    // Test...
+    public struct Address
+    {
+        public string Street { get; set; }
+        public int Building_number { get; set; }
+        public string City { get; set; }
+
+        /// <summary>
+        /// Simple constractor
+        /// </summary>
+        /// <param name="_street"></param>
+        /// <param name="_building_number"></param>
+        /// <param name="_city"></param>
+        public Address(string _street, int _building_number, string _city)
+        {
+            Street = _street;
+            Building_number = _building_number;
+            City = _city;
+        }
+
+        /// <summary>
+        /// To String function 
+        /// </summary>
+        /// <returns>Return the street, number and city in one string</returns>
+        public override string ToString() { return Street + " " + Building_number + ", " + City; }
+    }
---
+++ BE/Shop.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Shop
+{
+
+
+	public Shop()
+	{
+	}
+}
