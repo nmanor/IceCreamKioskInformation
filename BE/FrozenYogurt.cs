5a183e37c39521f71a9ebde810740671440cf3d5
mickeygoldmeier <45402517+mickeygoldmeier@users.noreply.github.com>
2020-03-19 18:44:09 +0200

    some more classes in BE waffle frozenyogurt frenchcrape and milktype enum

---
+++ BE/FrozenYogurt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    class FrozenYogurt : Product
+    {
+        private double _fat;
+        private MILKTYPE _milkType;
+
+
+        public override bool Search(Dictionary<string, List<object>> dictionary)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BE/Review.cs
+++ BE/Review.cs
@@ -3,8 +3,27 @@ namespace BE
 {
     public class Review
     {
-        public Review()
+        private string _reviewerName;
+        private DateTime _reviwerBirthday;
+        private string _review;
+        private int _rating;
+        private string _imageURL;
+        private DateTime _publishDate;
+
+        public Review(string reviewerName, DateTime reviwerBirthday, string review, int rating, string imageURL, DateTime publishDate)
         {
+            _reviewerName = reviewerName;
+            _reviwerBirthday = reviwerBirthday;
+            _review = review;
+            _rating = rating;
+            _imageURL = imageURL;
+            _publishDate = publishDate;
         }
+
+        public bool Search(string str)
+        { return true; }
+        public bool Search(DateTime date)
+        { return true; }
+
     }
 }
