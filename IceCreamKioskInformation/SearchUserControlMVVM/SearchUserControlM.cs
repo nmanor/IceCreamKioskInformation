9f4e9ad724007e7a1b45eb1e5412dacc3b683d48
mickeygoldmeier <45402517+mickeygoldmeier@users.noreply.github.com>
2020-05-05 10:35:40 +0300

    fix a bug in search product

--- IceCreamKioskInformation/SearchUserControlMVVM/SearchUserControlM.cs
+++ IceCreamKioskInformation/SearchUserControlMVVM/SearchUserControlM.cs
@@ -12,7 +12,8 @@ namespace IceCreamKioskInformation
             List<Product> result = new List<Product>();
             foreach(Product product in products)
             {
-                KeyValuePair<bool, Dictionary<string, List<object>>> search = product.Search(Dictionary);
+                Dictionary<string, List<object>> newDictionary = new Dictionary<string, List<object>>(Dictionary);
+                KeyValuePair<bool, Dictionary<string, List<object>>> search = product.Search(newDictionary);
                 if (search.Key)
                 {
                     if(search.Value.Count == 0)
